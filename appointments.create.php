9c39805fb76221d8c6b00e62da18a142f38d6715
besg <besguerra45@gmail.com>
2015-04-09 17:27:35 -0600

    Adding appointment form

--- appointments.create.php
+++ appointments.create.php
@@ -22,8 +22,26 @@ if($_SESSION["userType"] == "clerk")
 </div>
 
 <div class = "row">
-    
+     <form>
+        <div class="row"><h4 class="col-md-8">PATIENT</h4> </div><div class="row"><p class="col-md-4">
+                <select data-rel="chosen">
+                    <option></option> 
+                    <option>John Mark Green</option>
+                     <option>Simon Joseph Kim</option>
+                     <option>Beatrice Rose</option>
+                </select>
+            </p></div>
+        <hr>
+        <div class="row"><h4 class="col-md-6">APPOINTMENT INFORMATION</h4></div><div class="row"><p class="col-md-4">
+        Date: <input type="date" class="form-control">
+        Time: <input type="time" class="form-control">
+        Location: <input type="text" class="form-control">
+        Reason for Appointment: <input type="text" class="form-control">
+        </p></div>
+        <input class="btn btn-primary" type="submit" value="Create Appointment">
+    </form>
 </div>
+
 <?php
 include_once 'footer.php';
 ?>
