64aec982e82ca6e813c6eed2bf9242830f27f423
johnm7115 <johnm7115@gmail.com>
2015-04-09 16:49:29 -0600

    Fake patient info, empty medical history box.

--- patients.show.php
+++ patients.show.php
@@ -21,7 +21,7 @@ include_once 'menu.php';
     <div class="box">
         <div class="box-inner">
             <div class="box-header well" data-original-title="">
-                <h2><i class="glyphicon glyphicon-info-sign"></i> Patient Info</h2>
+                <h2><i class="glyphicon glyphicon-info-sign"></i> Simon Kim's Info</h2>
 <?php if($_SESSION["userType"] == "professional") { ?>
                 <div class="box-icon">
                     <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-down"></i></a>
@@ -35,8 +35,34 @@ include_once 'menu.php';
             </div>
             <div class="box-content">
 <?php } ?>
+                <p><b>Full Name:</b> Simon Joseph Kim</p>
+                <p><b>Alberta Health Care Number:</b> 1234-56789</p>
+                <p><b>Alberta Health Services ID:</b> 9876-54321</p>
+                <p><b>Birth date:</b> June 9, 1990</p>
+                <p><b>Address:</b> 123 Main St.</p>
+                <p><b>Postal Code:</b>T2G 2B4</p>
+                <p><b>Daytime Phone Number:</b> (403) 210 - 7625</p>
+                <p><b>Evening Phone Number:</b> (403) 555 - 1234</p>
+                <p><b>Email address:</b> sjkim@null.com</p>
+                <p><b>Family Physician Name:</b> Dr. Sean Wang</p>
                 <p class="text-right">
                     <a class="btn btn-info" href="patients.edit.php"><i class="glyphicon glyphicon-edit icon-white"></i>Edit</a>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="box">
+        <div class="box-inner">
+            <div class="box-header well" data-original-title="">
+                <h2><i class="glyphicon glyphicon-info-sign"></i> Medical History</h2>
+                <div class="box-icon">
+                    <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+                </div>
+            </div>
+            <div class="box-content">
+                <p class="text-right">
+                    <a class="btn btn-info" href="records.php"><i class="glyphicon glyphicon-eye-open icon-white"></i> View full history</a>
+                    <a href="records.create.php" class="btn btn-success"><i class="glyphicon glyphicon-plus-sign"></i> Create Record</a>
                 </p>
             </div>
         </div>
