b91498ffd7d8c370740e7d96c2fef3508c16f703
Navid Pourmomtaz <navid.pourmomtaz@gmail.com>
2015-04-09 12:13:13 -0600

    added calendar to clerk home page

--- footer.php
+++ footer.php
@@ -4,7 +4,7 @@
         <hr>
                 <footer class="row">
         
-            <p class="col-md-9 col-sm-9 col-xs-12 copyright">&copy; </p>
+            <p class="col-md-9 col-sm-9 col-xs-12 copyright">&copy; Team <strong>On Pointe</strong></p>
             <p class="col-md-3 col-sm-3 col-xs-12 powered-by">Powered by: 
                 <a href="http://usman.it/free-responsive-admin-template">Charisma</a></p>
         
