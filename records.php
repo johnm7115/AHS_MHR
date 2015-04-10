3523b0ab7624289478e94b181da1ed9e6eaf8375
besg <besguerra45@gmail.com>
2015-04-09 18:26:38 -0600

    Adding medical history data

--- records.php
+++ records.php
@@ -20,7 +20,24 @@ include_once 'menu.php';
 </div>
 
 <div class="row">
-    <h2>Medical history for ___</h2>
+    <h2 class="col-md-12">Medical History for Simon Kim</h2>
+    
+    <h4 class="col-md-10"> <a href="">Pain in kidneys </a></h4>
+    <h5 class="col-md-2">January 4, 2015 </h5>
+    
+    <h4 class="col-md-10"> <a href="">Abdominal pain and vomiting </a></h4>
+    <h5 class="col-md-2">October 1, 2014  </h5>
+    
+    <h4 class="col-md-10"> <a href="">Pain in right ankle </a></h4>
+    <h5 class="col-md-2">October 1, 2014  </h5>
+   
+    <h4 class="col-md-10"><a href="">High fever</a></h4>
+    <h5 class="col-md-2">December 1, 2011</h5>
+    
+    <h4 class="col-md-10"><a href="">Broken left hand</a></h4>
+    <h5 class="col-md-2">August 7, 2010</h5>
+
+    
 </div>
 <?php
 include_once 'footer.php';
