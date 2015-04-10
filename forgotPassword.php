adec90323dfb4e43994db56a7603c6c5d858b04d
johnm7115 <johnm7115@gmail.com>
2015-04-09 23:10:03 -0600

    Fixed? Forgot password layout.

--- forgotPassword.php
+++ forgotPassword.php
@@ -15,27 +15,28 @@
 </div>
 <!-- topbar ends -->
 
-<div class="row">
-	<div class="box col-md-12">
-		<div class="box-inner">
-			<div class="box-header well">
-				<h2><i class="glyphicon glyphicon-question-sign"></i> Forgot Password</h2>
-				<div class="box-icon">
-				<a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
-				<a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
-				<a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
-				</div>
-			</div>
-			<div class="box-content">
-				<p>Please enter your work email in the box below and you will receive instructions on how to change your password:</p>
-				<div class="input-group input-group-lg">
-					<span class="input-group-addon"><i class="glyphicon glyphicon-envelope red"></i></span>
-					<input id="inputIcon" type="text" class="form-control" placeholder="Your Email Address">
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<div class="ch-container">
+    <div class="row">
+        <div class="box col-md-12">
+            <div class="box-inner">
+                <div class="box-header well">
+                    <h2><i class="glyphicon glyphicon-question-sign"></i> Forgot Password</h2>
+                    <div class="box-icon">
+                    <a href="#" class="btn btn-setting btn-round btn-default"><i class="glyphicon glyphicon-cog"></i></a>
+                    <a href="#" class="btn btn-minimize btn-round btn-default"><i class="glyphicon glyphicon-chevron-up"></i></a>
+                    <a href="#" class="btn btn-close btn-round btn-default"><i class="glyphicon glyphicon-remove"></i></a>
+                    </div>
+                </div>
+                <div class="box-content">
+                    <p>Please enter your work email in the box below and you will receive instructions on how to change your password:</p>
+                    <div class="input-group input-group-lg">
+                        <span class="input-group-addon"><i class="glyphicon glyphicon-envelope red"></i></span>
+                        <input id="inputIcon" type="text" class="form-control" placeholder="Your Email Address">
+                    </div>
+                </div>
+            </div>
+
+
 
 
 <?php
