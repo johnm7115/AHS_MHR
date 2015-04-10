e6886115543b8db72f119b98ce5cb628b5ff8b61
johnm7115 <johnm7115@gmail.com>
2015-04-09 21:13:00 -0600

    Updated index.php

--- index.php
+++ index.php
@@ -18,23 +18,43 @@
             $attemptFailed = true;
         }
     }
+    
+    $pageTitle = "AHS HIMS";
+    include "./header.php"
 ?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php echo isset($attemptFailed) ? "Invalid Credentials." : ""; ?>
-        <!-- Temporary -->
-        <form method="POST">
-            Username:<br>
-            <input type="text" name="username">
-            <br>
-            Password:<br>
-            <input type="password" name="password">
-            <input type="submit" value="Submit">
-        </form>
-        <p>Use username {clerk, assistant, professional, ...} and any password to login.</p>
-    </body>
-</html>
+<!-- topbar starts -->
+<div class="navbar navbar-default" role="navigation">
+    <div class="navbar-inner">
+        <button type="button" class="navbar-toggle pull-left animated flip">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="/"><span>HIMS</span></a>
+    </div>
+</div>
+<!-- topbar ends -->
+
+<div class="col-md-offset-1 col-md-10">
+    <div class="row">
+        <div class="col-lg-6">
+            <h1>About Us</h1>
+            <p>
+                
+            </p>
+        </div>
+        <div class="col-lg-6" style="border-left: 3px solid #bbbbbb;">
+            <h1>Login</h1>
+            <?php echo isset($attemptFailed) ? "Invalid Credentials." : ""; ?>
+            <!-- Temporary -->
+            <form method="POST">
+                <label for="username">Username</label>
+                <input class="form-control" type="text" name="username">
+                <label for="password">Password</label>
+                <input class="form-control" type="password" name="password"><br />
+                <input class="btn btn-primary" type="submit" value="Submit">
+            </form>
+            <p>Use username {clerk, assistant, professional, ...} and any password to login.</p>
+        
+<?php include "footer.php"; ?>
